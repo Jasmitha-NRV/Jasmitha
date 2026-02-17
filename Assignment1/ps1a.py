d0ba0a87e9abbce11522a37e1bc6bfb9dc9ccfb3
Jasmitha-NRV <harishmithajasmitha@gmail.com>
2026-02-17 22:28:54 +0530

    Update ps1a.py

--- Assignment1/ps1a.py
+++ Assignment1/ps1a.py
@@ -1,16 +1,21 @@
-
+# Getting the required input from the user
 annual_salary   = float(input("Enter your annual salary: "))
 portion_saved   = float(input("Enter the percent of your salary to save, as a decimal: "))
 total_cost      = float(input("Enter the cost of your dream home: "))
 
+# Calculating monthly return on investment since we need to find the number of months
 monthly_return=0.04/12
+# Calculating the down payment required
 down_payment=0.25*total_cost
+# Initial savings is zero
 savings=0.0
+# Initial number of months is zero
 months=0
-
+# Taking mothly salary so that monthly return can be applied
 monthly_salary=annual_salary//12
 
-while savings<=down_payment:
+# When savings equals downpayment the loop needs to stop
+while savings<down_payment:
     months+=1
     
     savings+=savings*monthly_return    
