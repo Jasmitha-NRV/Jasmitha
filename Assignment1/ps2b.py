44d4dd720b180d8d98415d0f54a7143f215275a8
Jasmitha-NRV <harishmithajasmitha@gmail.com>
2026-02-17 22:28:26 +0530

    Update ps2b.py

--- Assignment1/ps2b.py
+++ Assignment1/ps2b.py
@@ -1,8 +1,9 @@
-
+# Getting the required data from the user
 annual_salary   = float(input("Enter your annual salary: "))
 portion_saved   = float(input("Enter the percent of your salary to save, as a decimal: "))
 total_cost      = int(input("Enter the cost of your dream home: "))
 semi_annualraise= float(input("Enter the semi-annual raise, as a decimal:"))
+
 
 monthly_return=0.04/12
 down_payment=0.25*total_cost
@@ -12,9 +13,10 @@ months=0
 monthly_salary=annual_salary/12
 
 while savings<=down_payment:
-
+# Since the salary is increased every six months,
+# If the nuber of months is divisible by 6 then the salary must increase
     if (months%6==0 and months>0):
-        annual_salary+=(annual_salary*semi_annualraise)
+        annual_salary *= (1+semi_annualraise)
         monthly_salary=annual_salary/12
     savings+=savings*monthly_return    
     savings+=monthly_salary*portion_saved
